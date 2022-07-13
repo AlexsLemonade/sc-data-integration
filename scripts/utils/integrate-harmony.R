3ebea8bd37525ff79215d3509c0ac8eac028c954
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-07-13 14:33:22 -0400

    Need harmony:: and load SCE

--- scripts/utils/integrate-harmony.R
+++ scripts/utils/integrate-harmony.R
@@ -1,4 +1,6 @@
-#' Integrate a combined SCE with Harmony
+library(SingleCellExperiment)
+
+#' Integrate a combined SCE with harmony
 #' 
 #' This function performs integration on a merged SCE object with different batches
 #'  using the harmony R package. It performs integration using two approaches within
@@ -9,7 +11,7 @@
 #' @param combined_sce A combined SCE object. Must contain a cell column `batch`
 #'   that indicates the different groups to be integrated.
 #'
-#' @return An integrated SCE object with two additional reducedDim names: 
+#' @return An integrated SCE object with two additional reducedDim fields: 
 #'   `harmony_pcs` and `harmony_gene_matrix`
 integrate_harmony <- function(combined_sce) {
   
@@ -18,15 +20,15 @@ integrate_harmony <- function(combined_sce) {
     stop("The combined_sce object must contain a cell data field `batch` to perform integration.")
   }
   
-  # Create metadata information for input to Harmony ---------------------------
+  # Create metadata information for input to harmony ---------------------------
   harmony_metadata <- tibble::tibble(
     cell_id = rownames(colData(combined_sce)),
     batch = colData(combined_sce)$batch
   ) 
   
   # Perform integration starting from PCs --------------------------------------
-  # In this case, Harmony uses pre-computed PCs during integration
-  harmony_from_pcs <- HarmonyMatrix(
+  # In this case, harmony uses pre-computed PCs during integration
+  harmony_from_pcs <- harmony::HarmonyMatrix(
     data_mat  = reducedDim(combined_sce, "PCA"), # Matrix with coordinates for each cell (row) along many PCs (columns)
     meta_data = harmony_metadata, 
     vars_use  = "batch", # column in meta_data that indicates groups to integrate
@@ -34,8 +36,8 @@ integrate_harmony <- function(combined_sce) {
   )
   
   # Perform integration starting from the expression matrix --------------------
-  # In this case, Harmony computes PCs to use during integration
-  harmony_from_gene_matrix <- HarmonyMatrix(
+  # In this case, harmony computes PCs to use during integration
+  harmony_from_gene_matrix <- harmony::HarmonyMatrix(
     data_mat  = logcounts(combined_sce), # Gene expression matrix
     meta_data = harmony_metadata, # Dataframe with information for each cell (row)
     vars_use  = "batch",
