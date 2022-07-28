cecd044919ad456bfdd7ca70dae85628396c422f
cbethell <chantebethell@gmail.com>
2022-07-28 16:16:55 -0500

    incorporate code review refactoring suggestions
    - update renv.lock file
    - add batch info to identity
    - add dims as function argument

--- scripts/utils/integrate-seurat.R
+++ scripts/utils/integrate-seurat.R
@@ -9,7 +9,13 @@ library(magrittr)
 #' @param seurat_list List of seurat objects normalized with Seurat::SCTransform()
 #' @param reduction_method The Seurat reduction method to implement with 
 #' Seurat::FindIntegrationAnchors(), can be "rpca" or "cca"
-#' @param num_features Number of variable features to use for integration
+#' @param num_genes Number of variable features to use for integration
+#' @param batch_column The column variable indicating batches, which would 
+#' typically corresponds to the library ID. Default is "batch".
+#' @param integration_dims The range of values to supply to the `dims` argument
+#' when implementing the integration functions
+#' @param umap_dims The range of values to supply to the `dims` argument when
+#' implementing the `runUMAP()` function. Default is 1:10.
 #'
 #' @return integrated seurat object
 #'
@@ -17,7 +23,11 @@ library(magrittr)
 
 integrate_seurat <- function(seurat_list,
                              reduction_method,
-                             num_features = 3000){
+                             num_genes = 3000,
+                             batch_column = "batch",
+                             integration_dims,
+                             umap_dims = c(1:10),
+                             ...){
   
   # check that all objects contain the `SCT` assay as default assay
   for(seurat_obj in seurat_list){
@@ -31,8 +41,13 @@ integrate_seurat <- function(seurat_list,
     }
   }
   
+  # check that the batch column exists
+  if (!(batch_column %in% names(seurat_obj@meta.data))) {
+    stop("The provided `batch_column` column must be in the seurat object's metadata.")
+  }
+  
   # find common variable features for integration
-  common_features <- Seurat::SelectIntegrationFeatures(seurat_list, nfeatures = num_features)
+  common_features <- Seurat::SelectIntegrationFeatures(seurat_list, nfeatures = num_genes)
   
   # find the shared genes across all objects
   shared_genes <- seurat_list %>%
@@ -47,24 +62,25 @@ integrate_seurat <- function(seurat_list,
   # find anchors and perform integration
   anchors <- Seurat::FindIntegrationAnchors(object.list = seurat_list,
                                             reduction = reduction_method,
-                                            dims = 1:50)
+                                            dims = integration_dims)
   
   
   integrated_object <- Seurat::IntegrateData(anchorset = anchors,
                                              normalization.method = "SCT",
                                              features.to.integrate = shared_genes,
-                                             dims = 1:50)
+                                             dims = integration_dims)
   
-  # set idents as library ID
-  Seurat::Idents(integrated_object) <- "library"
+  # set idents as the batch column
+  Seurat::Idents(integrated_object) <- batch_column
   
   # make sure that the default assay is set to integrated
   Seurat::DefaultAssay(integrated_object) <- "integrated"
   
   # add PCA and UMAP to the integrated object
   integrated_object <- integrated_object %>%
-    Seurat::RunPCA() %>%
-    Seurat::RunUMAP(reduction = "pca", dims = 1:10)
+    Seurat::RunPCA(reduction.name = paste0("seurat", reduction_method, "_PCA")) %>%
+    Seurat::RunUMAP(reduction.name = paste0("seurat", reduction_method, "_UMAP"),
+                    reduction = paste0("seurat", reduction_method, "_PCA"), dims = umap_dims)
   
   return(integrated_object)
   
