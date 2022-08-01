f96c0f71dabb706ad9526396b6d1ba40cf6af26a
Ally Hawkins <ally.hawkins@ccdatalab.org>
2022-08-01 15:49:19 -0500

    accommodate missing X assay

--- scripts/utils/test_integration-functions.R
+++ scripts/utils/test_integration-functions.R
@@ -82,9 +82,7 @@ cowplot::plot_grid(pre_integration, post_integration, ncol = 1)
 anndata_dir <- file.path(here::here(
   "results",
   "human_cell_atlas", 
-  "anndata",
-  "integrated_scanorama_objects"
+  "integrated_anndata_objects"
 ))
-scanorama_integrated_file <- file.path(anndata_dir, "1M_Immune_Cells_scanorama_integrated.h5")
-scanorama_integrated_sce <- zellkonverter::readH5AD(scanorama_integrated_file,
-                                                    X_name = "counts")
+scanorama_integrated_file <- file.path(anndata_dir, "1M_Immune_Cells_integrated_anndata_scanorama.h5")
+scanorama_integrated_sce <- zellkonverter::readH5AD(scanorama_integrated_file, skip_assays = TRUE)
