c173693db97d5cbf3c8710806d6865abbeb8cb7f
Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>
2022-08-04 12:00:44 -0500

    Apply suggestions from code review
    Co-authored-by: Joshua Shapiro <josh.shapiro@ccdatalab.org>

--- scripts/03c-integrate-scvi.py
+++ scripts/03c-integrate-scvi.py
@@ -21,11 +21,13 @@ from utils.integrate_scvi import integrate_scvi
 
 # define arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--input_anndata',
+parser.add_argument('-i', '--input_anndata',
                     dest = 'input_anndata',
+                    required = True,
                     help = 'Path to HDF5 file with merged AnnData object to integrate')
-parser.add_argument('-i', '--output_anndata',
+parser.add_argument('-o', '--output_anndata',
                     dest = 'output_anndata',
+                    required = True,
                     help = 'Path to HDF5 file to save the integrated AnnData object')
 parser.add_argument('-b', '--batch_column',
                     dest = 'batch_column',
@@ -76,8 +78,7 @@ if not file_ext.search(args.output_anndata):
 
 # check that output file directory exists and create directory if doesn't exist
 integrated_adata_dir = os.path.dirname(args.output_anndata)
-if not os.path.isdir(integrated_adata_dir):
-    os.makedirs(integrated_adata_dir, exist_ok = True)
+os.makedirs(integrated_adata_dir, exist_ok = True)
 
 # read in merged anndata object
 merged_adata = adata.read_h5ad(args.input_anndata)
@@ -113,10 +114,10 @@ else:
 # integrate anndata with scvi
 scvi_integrated_adata = integrate_scvi(merged_adata,
                                        integrate_genes = var_genes,
-                                       batch_column=args.batch_column,
-                                       categorical_covariate_columns=categorical_covariates,
-                                       continuous_covariate_columns=continuous_covariates,
-                                       seed = 2022)
+                                       batch_column = args.batch_column,
+                                       categorical_covariate_columns = categorical_covariates,
+                                       continuous_covariate_columns = continuous_covariates,
+                                       seed = args.seed)
 
 # remove raw data and logcounts to minimize space if corrected_only is true
 if args.corrected_only:
