987c05826493966246f8425eba526a3235df684b
Ally Hawkins <ally.hawkins@ccdatalab.org>
2022-08-16 16:13:05 -0500

    update argument names and set defaults

--- scripts/04-post-process-anndata.R
+++ scripts/04-post-process-anndata.R
@@ -3,35 +3,31 @@ project_root <- here::here()
 renv::load(project_root)
 
 # import libraries
-library(magrittr)
-library(optparse)
-library(SingleCellExperiment)
+suppressPackageStartupMessages(
+  library(magrittr)
+  library(optparse)
+  library(SingleCellExperiment) 
+)
 
 # Set up optparse options
 option_list <- list(
   make_option(
-    opt_str = c("--input_h5_file"),
+    opt_str = c("--input_anndata"),
     type = "character",
-    default = "results/human_cell_atlas/integrated_anndata/1M_Immune_Cells_integrated_scVI.h5",
+    default = NULL,
     help = "Path to H5 file that contains the integrated AnnData object"
   ),
   make_option(
     opt_str = c("--output_sce_file"),
     type = "character",
-    default = "results/human_cell_atlas/integrated_sce/1M_Immune_Cells_integrated_scVI_sce.rds",
+    default = NULL,
     help = "Path to RDS file where the integrated SCE object will be saved"
   ),
   make_option(
     opt_str = c("--method"),
     type = "character",
-    default = "scVI",
+    default = NULL,
     help = "Method that was used for integration, either `scanorama` or `scVI` (case-insensitive)."
-  ),
-  make_option(
-    opt_str = c("-b", "--batch_column"),
-    type = "character",
-    default = "batch",
-    help = "Name of the column in the AnnData object that indicates batch groupings."
   ),
   make_option(
     opt_str = c("--seed"),
@@ -52,6 +48,8 @@ option_list <- list(
 # Setup ------------------------------------------------------------------------
 # Parse options
 opt <- parse_args(OptionParser(option_list = option_list))
+
+set.seed(opt$seed)
 
 # Check provided method based on available methods
 available_methods <- c("scanorama", "scvi")
@@ -75,17 +73,17 @@ if (is.null(opt$method)) {
 }
 
 # Check that provided input file exists and is an RDS file
-if(is.null(opt$input_h5_file)) {
-  stop("You must provide the path to the H5 file with merged SCEs to --input_h5_file")
+if(is.null(opt$input_anndata)) {
+  stop("You must provide the path to the H5 file with merged SCEs to --input_anndata")
 } else {
-  if(!file.exists(opt$input_h5_file)) {
-    stop("Provided --input_h5_file file does not exist.")
+  if(!file.exists(opt$input_anndata)) {
+    stop("Provided --input_anndata file does not exist.")
   }
 }
 
 # Check that both input and output files have correct extensions
-if(!(stringr::str_ends(opt$input_h5_file, ".hdf5|.h5"))){
-  stop("`--input_h5_file` must end in either '.hdf5' or '.h5'")
+if(!(stringr::str_ends(opt$input_anndata, ".hdf5|.h5"))){
+  stop("`--input_anndata` must end in either '.hdf5' or '.h5'")
 }
 if(!(stringr::str_ends(opt$output_sce_file, ".rds"))){
   stop("`--output_sce_file` must end in '.rds'")
@@ -100,7 +98,7 @@ if(!dir.exists(integrated_sce_dir)){
 # Anndata to SCE post-processing -----------------------------------------------
 
 # read in AnnData object as H5
-integrated_sce <- zellkonverter::readH5AD(opt$input_h5_file, 
+integrated_sce <- zellkonverter::readH5AD(opt$input_anndata, 
                                           # if corrected only is set, skips reading `X` matrix
                                           skip_assays = opt$corrected_only)
 
