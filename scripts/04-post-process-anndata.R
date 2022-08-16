a3d93cc181096ffa85fadebc9a59c4cc71141405
Ally Hawkins <ally.hawkins@ccdatalab.org>
2022-08-16 16:29:41 -0500

    add post processing to snakefile

--- scripts/04-post-process-anndata.R
+++ scripts/04-post-process-anndata.R
@@ -3,11 +3,11 @@ project_root <- here::here()
 renv::load(project_root)
 
 # import libraries
-suppressPackageStartupMessages(
+suppressPackageStartupMessages({
   library(magrittr)
   library(optparse)
   library(SingleCellExperiment) 
-)
+})
 
 # Set up optparse options
 option_list <- list(
