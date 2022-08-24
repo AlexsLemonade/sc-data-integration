03015ee6f379155d71322c012bd37d398357b648
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-08-24 16:50:38 -0400

    file spacing

--- scripts/utils/calculate-pca-regression.R
+++ scripts/utils/calculate-pca-regression.R
@@ -10,7 +10,7 @@ source(
   )
 )
 
-#' Function to perform PCA regression for batch effect evaluation as described 
+#' Function to perform PCA regression for batch effect evaluation as described
 #'   in the `kBET` manuscript
 #'
 #' @param integrated_sce The integrated SCE object
@@ -19,32 +19,32 @@ source(
 #' @param integration_method The name of the method that was used for integration
 #'    to create `integrated_sce`. One of: fastMNN, harmony, rpca, cca, scvi, or scanorama
 #' @param num_pcs Number of PCs to consider during regression calculation. Default: 50
-#' @significance_threshold Threshold for considering a corrected P-value from PC~batch 
+#' @significance_threshold Threshold for considering a corrected P-value from PC~batch
 #'  regression as significant. Default: 0.05
 
 #'
-#' @return Tibble with three columns: `integration_method`,the given integration method; 
-#' `batch_variance`, the total contribution of the batch effect to the variance; 
+#' @return Tibble with three columns: `integration_method`,the given integration method;
+#' `batch_variance`, the total contribution of the batch effect to the variance;
 #' `pc_regression_scaled`, proxy for batch effect
-#' 
-calculate_pca_regression <- function(integrated_sce, 
-                                     batch_column = "batch", 
+#'
+calculate_pca_regression <- function(integrated_sce,
+                                     batch_column = "batch",
                                      num_pcs = 50,
                                      significance_threshold = 0.05,
                                      integration_method = NULL) {
-  
-  
+
+
   # Check integration method
   integration_method <- check_integration_method(integration_method)
-  
+
   # Get PCs or analagous reduced dimensions
   reduced_dim_name <- get_reduced_dim_name(integration_method)
   pcs <- reducedDim(integrated_sce, reduced_dim_name)
-  
+
   # Subset to given num_pcs PCs
   pca.data <- pcs[,1:num_pcs]
-  
-  
+
+
   perform_regression <- function(df) {
     # Adapted from https://github.com/theislab/kBET/blob/f35171dfb04c7951b8a09ac778faf7424c4b6bc0/R/kBET-utils.R#L168-L181
     # Note that we do not return this quantity: https://github.com/theislab/kBET/blob/f35171dfb04c7951b8a09ac778faf7424c4b6bc0/R/kBET-utils.R#L177
@@ -59,9 +59,9 @@ calculate_pca_regression <- function(integrated_sce,
       dplyr::select(r.squared, p.value)
     return(reg_result)
   }
-  
+
   # For each PC, regress against `batch` and obtain R^2 and associated P-value ----------
-  
+
   # Prepare data for modeling
   pc_tibble <- tibble::as_tibble(pca.data, rownames = "cellname") %>%
     # Pull out batch column to regress against
@@ -72,7 +72,7 @@ calculate_pca_regression <- function(integrated_sce,
     dplyr::mutate(PC_index = as.numeric(
       stringr::str_replace(PC_index, "^V", ""))
     )
-  
+
   # Perform regressions
   pc_regression_results <- pc_tibble %>%
     # Create nested data for each PC
@@ -82,37 +82,37 @@ calculate_pca_regression <- function(integrated_sce,
     # Perform regression PC~batch and unnest
     dplyr::mutate(fit = purrr::map(data, perform_regression)) %>%
     dplyr::select(-data) %>%
-    tidyr::unnest(fit) 
-  
-  
+    tidyr::unnest(fit)
+
+
   # Calculate metrics from regression  results --------------
   # All calculations are adapted from https://github.com/theislab/kBET/blob/master/R/pcRegression.R
-  
+
   # Calculate the "total contribution of the batch effect to the variance"
   pcs_variance <- pc_tibble %>%
-    dplyr::group_by(PC_index) %>% 
+    dplyr::group_by(PC_index) %>%
     dplyr::summarize(pc_variance = sd(PC)**2) %>%
     dplyr::pull(pc_variance)
-  
-  # Calculate \sum variance and the percentage explained by PCs 
-  sum_of_variance <- sum(pcs_variance) 
+
+  # Calculate \sum variance and the percentage explained by PCs
+  sum_of_variance <- sum(pcs_variance)
   explained_variance <- (pcs_variance / sum_of_variance)/100
-   
+
   # This is the approximation of: "the total contribution of the batch effect to the variance in the data" (pg 50, Methods section)
-  batch_variance <- sum(pc_regression_results$r.squared*explained_variance) 
-  
-  
+  batch_variance <- sum(pc_regression_results$r.squared*explained_variance)
+
+
   # Calculate the "sum of explained variance of all PCs with significant <R^2 from the batch regression>
   #  scaled by the variance explained by the top `num_pcs` PCs as a proxy for the batch effect" (pg 50, Methods section)
-  
-  # First, determine which corrected P-values are significant  
+
+  # First, determine which corrected P-values are significant
   corrected_pvalues_sig <- unname(p.adjust(pc_regression_results$p.value, method = 'BH')) < significance_threshold
-  
+
   # This the "proxy for the batch effect, scaled 0-1 where 0 = mo effect and 1 = strong effect
-  pc_reg_scale <- sum(explained_variance[corrected_pvalues_sig])/sum(explained_variance) 
-  
-  
-  
+  pc_reg_scale <- sum(explained_variance[corrected_pvalues_sig])/sum(explained_variance)
+
+
+
   # Return a 1-row tibble -----
   return(
     tibble::tibble(
@@ -121,5 +121,5 @@ calculate_pca_regression <- function(integrated_sce,
       pc_regression_scaled = pc_reg_scale
     )
   )
-  
+
 }
