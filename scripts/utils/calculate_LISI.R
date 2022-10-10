a67ddc06dfafed669b84e6cfe525fffcb0a58bba
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-10-10 09:17:48 -0400

    Update lisi function for more general batch or cell type calculation

--- scripts/utils/calculate_LISI.R
+++ scripts/utils/calculate_LISI.R
@@ -13,16 +13,16 @@ source(
   )
 )
 
-#' Function to calculate iLISI scores from an integrated SCE object
+#' Function to calculate LISI, either iLISI or cLISI, scores from an integrated SCE object
 #'
 #' @param integrated_sce The integrated SCE object
-#' @param batch_column The variable in `integrated_sce` indicating batches. Default
-#'   is "batch".
+#' @param batch_column The variable in `integrated_sce` indicating the grouping of interest.
+#'  Generally this is either batches or cell types. Default is "batch".
 #' @param integration_method The name of the method that was used for integration 
 #' @param unintegrated Indicates whether the provided data is integrated (`FALSE`; default) or
 #'   integrated (`TRUE`).
 #'   
-#' @return Tibble with five columns with one row per cell. Columns are `ilisi_score`, 
+#' @return Tibble with five columns with one row per cell. Columns are `lisi_score`, 
 #'   `cell_name` (combined barcode and library), `cell_barcode`, `library` and 
 #'   `integration_method`
 calculate_ilisi <- function(integrated_sce,
@@ -49,19 +49,19 @@ calculate_ilisi <- function(integrated_sce,
   pcs <- reducedDim(integrated_sce, reduced_dim_name)
   
   # Create data frame with batch information to provide to `compute_lisi()`
+  # Here, `batch` will refer to whatever the specified batch_column was, even if cell types
   batch_df <- data.frame(batch = colData(integrated_sce)[,batch_column])
   
   
   # `lisi_result` is a tibble with per-cell scores, the score roughly means:
   #   "how many different categories are represented in the local neighborhood of the given cell?"
-  #   With 2 batches, then, values close to 2 indicate good integration
   lisi_result <- lisi::compute_lisi(pcs, 
                                     # define the batches
                                     batch_df, 
                                     # which variables in `batch_df` to compute lisi for
                                     "batch") %>% 
     tibble::as_tibble() %>%
-    # Rename the result column to `ilisi_score`
+    # Rename the result column to `lisi_score`
     dplyr::rename(ilisi_score = batch) %>%
     # Add in the cell, library ID, and integration method
     dplyr::mutate(cell_name = colnames(integrated_sce),
