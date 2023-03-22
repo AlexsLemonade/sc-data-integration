d861dffda46455e01aeb08991c5ced768eda0eae
Ally Hawkins <ally.hawkins@ccdatalab.org>
2023-03-22 17:33:08 -0500

    simplify everything

--- scripts/utils/calculate-ARI.R
+++ scripts/utils/calculate-ARI.R
@@ -116,16 +116,22 @@ calculate_ari <- function(integrated_sce,
 #'
 calculate_reverse_ari <- function(individual_sce_list,
                                   integrated_sce,
-                                  num_pcs = 20,
                                   seed = NULL,
-                                  k_range = seq(5, 25, 5),
                                   integration_method = NULL,
                                   unintegrated = FALSE,
                                   batch_column = "batch"){
   set.seed(seed)
   
-  if(is.null(names(individual_sce_list))){
+  # check that list of SCE objects is named
+  batch_ids <- names(individual_sce_list)
+  if(is.null(batch_ids)){
     stop("Must provide a named list of SCE objects.")
+  } else {
+    # make sure the batch ids provided match between the list and the integrated object 
+    if(!all(batch_ids %in% unique(colData(integrated_sce)[, batch_column]))){
+      stop("Names of provided SCE objects included in the individual SCE object 
+           do not match batch IDs present in the batch_column of the integrated object")
+    }
   }
   
   # Settings depending on whether data is integrated or not
@@ -142,6 +148,7 @@ calculate_reverse_ari <- function(individual_sce_list,
     # Get name for reduced dimensions
     reduced_dim_name <- get_reduced_dim_name(integration_method)
   }
+
   
   # get pcs for individual objects
   ind_pcs <- purrr::map(individual_sce_list, get_individual_pcs)
@@ -149,69 +156,37 @@ calculate_reverse_ari <- function(individual_sce_list,
   # Pull out the PCs or analogous reduction, and remove batch NAs along the way
   integrated_pcs <- reducedDim(integrated_sce, reduced_dim_name)
   
-  # Set up parameters
-  frac_cells <- 0.8        # fraction of cells to downsample to
-  nreps <- 20              # number of times to repeat sub-sampling procedure
+  # cluster integrated pcs only one time
+  integrated_clustering_result <- bluster::clusterRows(integrated_pcs, 
+                                                       bluster::NNGraphParam(cluster.fun = "louvain",
+                                                                             type = "jaccard",
+                                                                             k = k)) |> 
+    set_names(rownames(integrated_pcs)) # make sure to set the names with the batch ids
   
-  # perform calculations
-  all_ari <- c()
-  for (i in 1:nreps) {
-    
-    # Downsample each individual pc matrix
-    ind_downsampled_pcs <- purrr::map(ind_pcs, \(pcs) downsample_pcs_for_metrics(pcs, frac_cells, num_pcs)) 
-    
-    # downsample integrated pcs so that equivalent fraction of cells from each batch is represented
-    # this is required so that the final cluster assignments contain the equivalent number of cells as 
-    # the original batch
-    integrated_downsampled_pcs <- integrated_pcs |>
-      as.data.frame() |>
-      tibble::rownames_to_column("cell_id") |> 
-      dplyr::mutate(batch = stringr::word(cell_id, -1, sep = "-")) |> 
-      dplyr::group_by(batch) |>
-      dplyr::slice_sample(prop = frac_cells) |> 
-      dplyr::ungroup() |> 
-      tibble::column_to_rownames("cell_id") |> 
-      dplyr::select(-"batch") |> 
-      as.matrix()
-    
   
-    for (k in k_range) {
-      # only cluster integrated pcs once for each k
-      integrated_clustering_result <- kmeans(x = integrated_downsampled_pcs, centers=k)
+  # for every batch id, cluster and then calculate ari for that batch 
+  all_ari <- batch_ids |> 
+    purrr::map_chr(\(batch){
       
-      # repeat clustering for each individual pc matrix
-      all_ari <- c(all_ari, 
-                   purrr::imap_chr(ind_downsampled_pcs, 
-                                 \(downsampled_pcs, batch_id){
-                                   
-                                   # grab clusters for cells from specified batch id
-                                   clusters_to_keep <- grep(batch_id, names(integrated_clustering_result$cluster))
-                                   batch_integrated_clusters <- unname(integrated_clustering_result$cluster[clusters_to_keep])
-                                   
-                                   # Perform k-means clustering for individual pcs
-                                   ind_clustering_result <- kmeans(x = downsampled_pcs$pcs, centers=k)
-                                   ind_clusters <- unname(ind_clustering_result$cluster)
-                                   
-                                   # Calculate reverse ARI
-                                   ari <- pdfCluster::adj.rand.index(batch_integrated_clusters, ind_clusters)
-                                   
-                                 }))
+      ind_clustering_result <- bluster::clusterRows(ind_pcs[[batch]],
+                                                    bluster::NNGraphParam(cluster.fun = "louvain",
+                                                                          type = "jaccard",
+                                                                          k = k))
+      # extract clusters from integrated clustering for batch 
+      clusters_to_keep <- grep(batch, names(integrated_clustering_result))
+      batch_integrated_clusters <- integrated_clustering_result[clusters_to_keep]
       
-    }
-  }
+      ari <- bluster::pairwiseRand(ind_clustering_result, 
+                                   batch_integrated_clusters, 
+                                   mode = "index")
+      
+    })
   
-  # grab length of batch ids
-  batch_id_length <- length(unique(names(all_ari)))
-  
-  # Create tibble with ari, batch id, and corresponding information
-  ari_tibble <- tibble::tibble(
+  # create data frame with ari and batch id
+  ari_df <- data.frame(
     ari = all_ari,
-    batch_id = names(all_ari),
-    rep = rep(1:nreps, each=length(k_range)*batch_id_length),
-    k   = rep(k_range, nreps*batch_id_length),
-    integration_method = integration_method
+    batch_id = batch_ids
   )
-  
-  # Return tibble with batch ARI results which can further be summarized downstream
-  return(ari_tibble)
+
+  return(ari_df)
 }
