ef504eaec62c7a5c94d00ea8f19c010ddb212577
Ally Hawkins <ally.hawkins@ccdatalab.org>
2023-03-22 11:36:14 -0500

    function for calculating reverse ARI

--- scripts/utils/calculate-ARI.R
+++ scripts/utils/calculate-ARI.R
@@ -3,6 +3,9 @@ suppressPackageStartupMessages({
   library(SingleCellExperiment)
 })
 
+# bring in any helper functions
+source(file.path("scripts", "utils", "integration-helpers.R"))
+
 #' Function to calculate ARI scores from an integrated SCE object
 #'
 #' This function uses a similar approach as used in this paper:
@@ -91,4 +94,127 @@ calculate_ari <- function(integrated_sce,
   # Return tibble with batch ARI results which can further be summarized downstream
   return(ari_tibble)
 
+}
+
+#' Calculate Reverse ARI to compare clustering pre and post integration
+#'
+#' @param individual_sce_list List of SCE objects containing all batches to use for 
+#'   calculating reverse ARI. Must be the original, pre-merged object.
+#'   The list must be a named list with the batch names.
+#' @param integrated_sce The integrated SCE object
+#' @param num_pcs The number of PCs to use during k-means clustering. Default: 20
+#' @param seed Seed for initializing random sampling and k-means clustering
+#' @param k_range Range of k values to use for k-means clustering. Default: `seq(5, 25, 5)`
+#' @param integration_method The name of the method that was used for integration
+#'    to create `integrated_sce`. One of: fastMNN, harmony, rpca, cca, scvi, or scanorama
+#' @param unintegrated Indicates whether the provided data is integrated (`FALSE`; default) or
+#'   integrated (`TRUE`).
+#' @param batch_column The variable in `integrated_sce` indicating the grouping of interest.
+#'  Generally this is either batches or cell types. Default is "batch".
+#'
+#' @return Tibble with five columns: `rep`, representing the given downsampling replicate;
+#'   `k`, the given k for k-means; `ari`, the calculated ARI for the given
+#'   combination of `rep` and `k`; the corresponding `batch_id` found in the `batch_column` 
+#'   for the original SCE object, and the `integration_method`, the provided integration method
+#'
+calculate_reverse_ari <- function(individual_sce_list,
+                                  integrated_sce,
+                                  num_pcs = 20,
+                                  seed = NULL,
+                                  k_range = seq(5, 25, 5),
+                                  integration_method = NULL,
+                                  unintegrated = FALSE,
+                                  batch_column = "batch"){
+  set.seed(seed)
+  
+  if(is.null(names(individual_sce_list))){
+    stop("Must provide a named list of SCE objects.")
+  }
+  
+  # Settings depending on whether data is integrated or not
+  if (unintegrated){
+    # In the end, we'll return "unintegrated" in the data frame for integration method
+    integration_method <- "unintegrated"
+    
+    # use simply "PCA" for reduced dimensions
+    reduced_dim_name <- "PCA"
+  } else {
+    # Check integration method
+    integration_method <- check_integration_method(integration_method)
+    
+    # Get name for reduced dimensions
+    reduced_dim_name <- get_reduced_dim_name(integration_method)
+  }
+  
+  # get pcs for individual objects
+  ind_pcs <- purrr::map(individual_sce_list, get_individual_pcs)
+  
+  # Pull out the PCs or analogous reduction, and remove batch NAs along the way
+  integrated_pcs <- reducedDim(integrated_sce, reduced_dim_name)
+  
+  # Set up parameters
+  frac_cells <- 0.8        # fraction of cells to downsample to
+  nreps <- 2              # number of times to repeat sub-sampling procedure
+  
+  # perform calculations
+  all_ari <- c()
+  for (i in 1:nreps) {
+    
+    # Downsample each individual pc matrix
+    ind_downsampled_pcs <- purrr::map(ind_pcs, \(pcs) downsample_pcs_for_metrics(pcs, frac_cells, num_pcs)) 
+    
+    # downsample integrated pcs so that equivalent fraction of cells from each batch is represented
+    # this is required so that the final cluster assignments contain the equivalent number of cells as 
+    # the original batch
+    integrated_downsampled_pcs <- integrated_pcs |>
+      as.data.frame() |>
+      tibble::rownames_to_column("cell_id") |> 
+      dplyr::mutate(batch = stringr::word(cell_id, -1, sep = "-")) |> 
+      dplyr::group_by(batch) |>
+      dplyr::slice_sample(prop = frac_cells) |> 
+      dplyr::ungroup() |> 
+      tibble::column_to_rownames("cell_id") |> 
+      dplyr::select(-"batch") |> 
+      as.matrix()
+    
+  
+    for (k in k_range) {
+      # only cluster integrated pcs once for each k
+      integrated_clustering_result <- kmeans(x = integrated_downsampled_pcs, centers=k)
+      
+      # repeat clustering for each individual pc matrix
+      all_ari <- c(all_ari, 
+                   purrr::imap_chr(ind_downsampled_pcs, 
+                                 \(downsampled_pcs, batch_id){
+                                   
+                                   # grab clusters for cells from specified batch id
+                                   clusters_to_keep <- grep(batch_id, names(integrated_clustering_result$cluster))
+                                   batch_integrated_clusters <- unname(integrated_clustering_result$cluster[clusters_to_keep])
+                                   
+                                   # Perform k-means clustering for individual pcs
+                                   ind_clustering_result <- kmeans(x = downsampled_pcs$pcs, centers=k)
+                                   ind_clusters <- unname(ind_clustering_result$cluster)
+                                   
+                                   # Calculate reverse ARI
+                                   ari <- pdfCluster::adj.rand.index(batch_integrated_clusters, ind_clusters)
+                                   
+                                 }))
+      
+    }
+  }
+  
+  # grab length of batch ids
+  batch_id_length <- length(unique(names(all_ari)))
+  
+  # Create tibble with ari, batch id, and corresponding information
+  ari_tibble <- tibble::tibble(
+    ari = all_ari,
+    batch_id = names(all_ari),
+    rep = rep(1:nreps, each=length(k_range)*batch_id_length),
+    k   = rep(k_range, nreps*batch_id_length),
+    integration_method = integration_method
+  )
+  
+  # Return tibble with batch ARI results which can further be summarized downstream
+  return(ari_tibble)
 }
